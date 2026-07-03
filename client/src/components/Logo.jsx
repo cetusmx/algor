d53f4258a5a087b99b9a6eb8797d1370f6d3a8fc
Oscar Rodriguez <118637313+cetusmx@users.noreply.github.com>
2026-07-03 14:57:09 -0600

    fix: crop internal whitespace in Logo SVG viewBox

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo({ className = '' }) {
   return (
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
-      viewBox="0 0 240 60" 
+      viewBox="10 10 170 40" 
       className={`w-32 h-auto ${className}`}
     >
       <polygon points="12,48 30,12 42,12 24,48" className="fill-current" />
